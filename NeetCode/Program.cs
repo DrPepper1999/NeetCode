f77c31f3a0f68ffae3f8af70cdda09fb93d1989e
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2025-05-01 02:13:13 +0300

    BestTimeToBuyAndSellStock

--- NeetCode/Program.cs
+++ NeetCode/Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using NeetCode;
+using NeetCode.SlidingWindow;
 
 
+var result = BestTimeToBuyAndSellStock.MaxProfit([10, 1, 5, 6, 7, 1]);
 
-DailyTemperaturesTask.DailyTemperatures([30, 38, 30, 36, 35, 40, 28]);
+Console.WriteLine(result);
